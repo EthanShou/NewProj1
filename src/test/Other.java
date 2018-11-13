2858ea3ba263cf6bd349845569f2ba637d9bbb7b
EthanShou <352185883@qq.com>
2018-11-13 14:34:09 +0800

    add Other.java and Test.java

---
+++ src/test/Other.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by admin on 2018/11/13.
+ */
+public class Other {
+}
---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by admin on 2018/11/13.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hhh");
+    }
+}
